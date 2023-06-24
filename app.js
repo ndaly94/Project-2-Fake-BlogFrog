05e7795ef43a8123d9bade171d8ce2781dcd46c1
NDaly94 <NDaly94@gmail.com>
2023-06-24 10:51:07 -0400

    app.js and server.js setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const morgan = require('morgan')
+const userRoutes = require('./routes/users')
+const postRoutes = require('./routes/posts')
+const app = express()
+
+app.use(express.json())
+app.use(morgan('combined'))
+app.use('/users', userRoutes)
+app.use('/posts', postRoutes)
+
+module.exports= app
---
+++ readMe.md
@@ -0,0 +1,5 @@
+Dependencies needed to run:
+
+Express, Node, dotenv, MogoURI, SECRET, MogoDB, Mongoose, bcrypt, jwt, morgan, 
+To run in dev mode nodemon must be installed globally. To run "DEV" type 'npm run dev' in the terminal.
+To start the app run 'npm start'
