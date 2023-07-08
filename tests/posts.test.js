59f07f01d9f2e07b82e973f3537007bc4410213b
NDaly94 <NDaly94@gmail.com>
2023-07-07 20:08:00 -0400

    testing updated to fix port issue

--- tests/posts.test.js
+++ tests/posts.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const app = require('../app');
-const server = app.listen(8080, () => { 
-    console.log("Testing on port 8080") });
+const server = app.listen(8081, () => { 
+    console.log("Testing on port 8082") });
 const Post = require('../models/post');
 const User = require('../models/user');
 let mongoServer;
